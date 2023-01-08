f088db39c4143603dce48e984eb361d9edb5701a
Ebenezer Ampiaw Yeboah <yebenezer64@gmail.com>
2023-01-08 06:56:03 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints upper and lower cases
+ * Return: always 0
+ */
+int main(void)
+{
+	char z = 122;
+	char Z = 90;
+	char a;
+	char A;
+
+	for (a = 97; a <= z; a++)
+		putchar(a);
+	for (A = 65; A <= 90; A++)
+		putchar(A);
+	putchar('\n');
+	return (0);
+}
