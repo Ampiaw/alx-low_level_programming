d0f8d65bcecc7a977f8d25b2a8f30d282b7ff040
Ebenezer Ampiaw Yeboah <yebenezer64@gmail.com>
2023-01-10 12:24:48 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - function that computes the absolute value of int
+ * @t: integer inputed
+ * Return: always 0
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
