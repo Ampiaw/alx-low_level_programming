fbabeb53a19f739471349b257d285cdae8e803df
Ebenezer Ampiaw Yeboah <yebenezer64@gmail.com>
2023-01-10 13:22:01 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit
+ * @i: integer
+ * Return: last digit
+ */
+int print_last_digit(int i)
+{
+	int l;
+
+	l = i % 10;
+	return (l);
+}
